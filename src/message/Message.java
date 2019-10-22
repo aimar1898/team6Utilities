a6f091c14655c2a69395644cbbe82842ec218876
fanfu <fanfu@DESKTOP-K965IRE>
2019-10-22 20:10:17 +0200

    First comitt. Built the project and added JavaDoc code on the indicated clases.

---
+++ src/message/Message.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package message;
+
+import user.User;
+
+/**
+ * The class will make the structure that will be sent via socket to comunicate client and server.
+ * @author aimar
+ */
+public class Message {
+    
+    private User user;
+    private String type;
+
+    /**
+     * @return the user
+     */
+    public User getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    /** 
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+}
